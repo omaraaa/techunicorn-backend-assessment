8c4297342b2017ef68721d4233f98da45bfd1306
Omar A. Alhammadi <emarati.no1@gmail.com>
2022-03-03 22:39:33 +0400

    db::registerAccount Implemented + schema change

--- src/api.rs
+++ src/api.rs
@@ -2,38 +2,10 @@ use rocket::serde::{json::Json, Deserialize, Serialize};
 
 use crate::db;
 
-#[derive(Serialize, Deserialize)]
-pub enum AccountType {
-    Patient,
-    Doctor,
-    Admin,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Doctor {
-    id: i64,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct RegisterForm {
-    name: String,
-    account_type: AccountType,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct LoginForm {
-    name: String,
-    account_type: AccountType,
-}
-
-#[post("/register", data = "<form>")]
-pub fn register(form: Json<RegisterForm>) -> Result<(), ()> {
-    todo!()
-}
-
-#[post("/login", data = "<form>")]
-pub fn login(form: Json<LoginForm>) -> Result<(), ()> {
-    todo!()
+#[post("/register", data = "<data>")]
+pub fn register(data: Json<db::RegisterData>) -> Result<(), ()> {
+    db::registerAccount(data.0).unwrap();
+    Ok(())
 }
 
 #[get("/doctors")]
--- src/db.rs
+++ src/db.rs
@@ -1,16 +1,77 @@
-use rusqlite::{params, Connection, Result};
+use rusqlite::{params, Connection};
 
-pub fn init_db() -> Result<()> {
+use derive_more::From;
+use serde::{Deserialize, Serialize};
+use std::result::Result;
+
+#[derive(Debug, From)]
+pub enum Error {
+    DBError(rusqlite::Error),
+    HashingError(argon2::Error),
+}
+
+pub fn init_db() -> Result<(), Error> {
     let conn = Connection::open("./database.db3")?;
-    conn.execute(include_str!("./schema.sql"), [])?;
+    conn.execute_batch(include_str!("./schema.sql"))?;
 
     Ok(())
 }
 
-pub fn doctors() -> Result<Vec<i32>> {
+pub fn doctors() -> Result<Vec<i32>, Error> {
     let conn = Connection::open("./database.db3")?;
     let mut stmt = conn.prepare("SELECT id FROM doctor")?;
-
     let q = stmt.query_map([], |row| row.get::<usize, i32>(0))?;
-    Ok(q.map(|f| f.unwrap()).collect())
+    Ok(q.collect::<Result<_, _>>()?)
+}
+
+pub struct Account {
+    email: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum AccountType {
+    Patient,
+    Doctor,
+    Admin,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RegisterData {
+    name: String,
+    email: String,
+    password: String,
+    account_type: AccountType,
+}
+
+use argon2::{self, Config};
+
+pub fn registerAccount(data: RegisterData) -> Result<(), Error> {
+    let conn = Connection::open("./database.db3")?;
+
+    let salt = b"should be random";
+    let config = Config::default();
+    let passhash = argon2::hash_encoded(data.password.as_bytes(), salt, &config)?;
+
+    let mut stmnt = conn.prepare(
+        "INSERT INTO account (fullname, email, passhash) VALUES (?1, ?2, ?3) RETURNING id",
+    )?;
+
+    let mut itr = stmnt.query_map(params![data.name, data.email, passhash], |r| {
+        r.get::<_, i32>(0)
+    })?;
+    let id = itr.next().unwrap()?;
+
+    match data.account_type {
+        Doctor => {
+            registerDoctor(id)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn registerDoctor(id: i32) -> Result<(), Error> {
+    let conn = Connection::open("./database.db3")?;
+    conn.execute("INSERT INTO doctor(id) values (?1)", params![id])?;
+    Ok(())
 }
